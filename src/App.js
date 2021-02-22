192280500fd81263b9dbcbe621ded0101669e51f
Levi Winans <levieiko@gmail.com>
2021-02-22 13:51:00 -0800

    export default class App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      HELLO WORLD
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {}
+  render(){
+    return (
+      <div className="App">
+        HELLO WORLD
+      </div>
+    );
+  };
+};
 
-export default App;
+
