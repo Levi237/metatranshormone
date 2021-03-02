4192ebf202ccab13c8542201f5efea07416ab354
Levi Winans <levieiko@gmail.com>
2021-03-01 20:16:14 -0800

    add nav bar

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { NavLink }          from 'react-router-dom';
+import styled               from 'styled-components';
+import * as routes          from '../../constants/routes';
+
+export default class NavBar extends Component {
+
+    render(){
+        return(
+            <LocalWrapper>
+                <LogoWrapper>
+                    <img src="logo512.png"/>
+                </LogoWrapper>
+                <NavWrapper>
+                    <NavLink activeClassName="nav-active" to={routes.HOME} >HOME</NavLink>
+                    <NavLink activeClassName="nav-active" to={routes.SERV} >SERVICES</NavLink>
+                    <NavLink activeClassName="nav-active" to={routes.INFO} >ABOUT</NavLink>
+                    <NavLink activeClassName="nav-active" to={routes.MAIL} >CONTACT</NavLink>
+                </NavWrapper>
+            </LocalWrapper>
+        )
+    }
+}
+
+const LocalWrapper = styled.div`
+    position: absolute;
+    width: 100vw;
+    height: 120px;
+    max-height: 120px;
+`;
+const LogoWrapper = styled.div`
+    display: inline-block;
+    img{
+        width: 80px;
+        margin: 20px;
+    }
+`;
+const NavWrapper = styled.div`
+    display: inline-block;
+    width: calc(100vw - 120px);
+    text-align: right;
+    vertical-align: top;
+    font-size: 20px;
+    line-height: 20px;
+    margin-top: 40px;
+    a {
+        margin-right: 40px;
+        text-decoration: none;
+        &:hover {
+            cursor: pointer;
+        }
+    }
+`;
