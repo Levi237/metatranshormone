fbd7c5555711c13af75764ea11a9c38cee6355b5
Levi Winans <levieiko@gmail.com>
2021-03-01 18:55:24 -0800

    add switches, organize imports

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Switch, Route }    from 'react-router-dom';
+import styled               from 'styled-components';
+
+import * as routes          from './constants/routes';
 
 export default class App extends Component {
   state = {}
   render(){
     return (
-      <div className="App">
-        HELLO WORLD
-      </div>
+      <AppContainer>
+        <Switch>
+            <Route path={routes.HOME} exact render={() => <><h1>HELLO HOME</h1></>} />
+            <Route path={routes.ROOT} render={() => <><h1>HELLO ROOT</h1></>} />
+        </Switch>
+        
+      </AppContainer>
     );
   };
 };
+
+const AppContainer = styled.div`
+  h1,h2,h3,ul {
+    margin-block-start: 0;
+    margin-block-end: 0;
+    line-height: 100%;
+  }
+  h1 {
+    text-align: center;
+    text-transform: uppercase;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import './index.css';
+import React                        from 'react';
+import ReactDOM                     from 'react-dom';
+import { BrowserRouter as Router }  from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import reportWebVitals              from './reportWebVitals';
 
 
 // import firebase from 'firebase';
