3d498f3a4e0d63cabb010f1c45643a8189cd6ebe
Levi Winans <levieiko@gmail.com>
2021-03-01 20:51:49 -0800

    add styled hamburger icon

--- src/components/nav/NavHamburger.js
+++ src/components/nav/NavHamburger.js
@@ -1,48 +1,41 @@
 import React       from 'react';
 import styled      from 'styled-components';
 
-const NavHamburger = ({ toggleMenu, pageStyle }) => {
-  return(<>
-    <Logo src="../logo.png" style={{display: pageStyle.displayLogo, filter: pageStyle.logoColor}}/>
+import { screens } from '../../constants/screens';
+
+const NavHamburger = ({ toggleMenu }) => {
+  return(
     <Hamburger name="toggleMenu" onClick={(e) => toggleMenu(e)}>
-      <section style={{backgroundColor: pageStyle.color}}/>
-      <section style={{backgroundColor: pageStyle.color}}/>
-      <section style={{backgroundColor: pageStyle.color}}/>
+      <section/>
+      <section/>
+      <section/>
     </Hamburger>
-  </>);
+  );
 };
 
-const Logo = styled.img`
-  position: absolute;
-  z-index:999;
-  left:0;
-
-  width: 36px;
-  margin: 5vw 0 0 5vw;
-`;
-const Title = styled.h1`
-  position: absolute;
-  z-index:991;
-width: 100vw!important;
-text-align:center;
-  color: white;
-text-transform: uppercase;
-  width: 36px;
-  margin: 60px auto!important;
-`;
 const Hamburger = styled.div`
   position: absolute;
   z-index: 999;
   right: 0;
-
-  margin: 5vw 5vw 0 0;
+  margin: -10px 35px 0 0;
 
   section {
     margin: 6px 0;
     width: 35px;
     height: 4px;
+    &:first-of-type{
+      background-color: var(--pink);
+    }
+    &:nth-of-type(2){
+      background-color: #fff;
+    }
+    &:last-of-type{
+      background-color: var(--pink);
+    }
   }
-  @media screen and (max-width: 945px) {
+  
+  @media screen and ( min-width: ${screens.mobile} ) {
+      display: none;
   }
 `;
 
