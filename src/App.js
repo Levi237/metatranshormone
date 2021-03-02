31d374887f58f1a88361118e595a923b6643187b
Levi Winans <levieiko@gmail.com>
2021-03-01 20:52:39 -0800

    add toggle script for nav menu

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import { Switch, Route }    from 'react-router-dom';
 import styled               from 'styled-components';
 
 import * as routes          from './constants/routes';
+import { screens }          from './constants/screens';
 
 import NavBar               from './components/nav/NavBar';
+import NavMenu              from './components/nav/NavMenu';
 
 export default class App extends Component {
   state = {}
+  toggleMenu = () => {
+    const hamburgerMenu = document.getElementById('menu');
+    hamburgerMenu.classList.toggle('active');
+    hamburgerMenu.classList.toggle('inactive');
+  };
   render(){
     return (
       <AppContainer>
-        <NavBar/>
+        <NavMenu toggleMenu={this.toggleMenu}/>
+        <NavBar toggleMenu={this.toggleMenu}/>
         <Switch>
             <Route path={routes.HOME} exact render={() => <FullScreenContainer><h1>HELLO HOME</h1></FullScreenContainer>} />
             <Route path={routes.ROOT} render={() => <FullScreenContainer><h1>HELLO ROOT</h1></FullScreenContainer>} />
@@ -35,6 +43,7 @@ const AppContainer = styled.div`
 `;
 const FullScreenContainer = styled.div`
   width: 100vw;
-  height: 100vh;
+  height: calc( 100vh - 120px );
+  padding-top: 120px;
   background: var(--blue);
 `;
