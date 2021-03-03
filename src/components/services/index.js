7efaf34b9e743c03a774721380a06224b5fea357
Levi Winans <levieiko@gmail.com>
2021-03-03 03:37:00 -0800

    new page for services

---
+++ src/components/services/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+// import { Link } from 'react-router-dom';
+
+export default class Services extends Component {
+    render(){
+        return(
+            <LocalWrapper>
+                <h1>Our Services</h1>
+            </LocalWrapper>
+        )
+    }
+}
+
+const LocalWrapper = styled.div`
+`;
